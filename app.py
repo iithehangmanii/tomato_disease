1de554792f944155ccf6d3e2773a5a0926078dff
iithehangmanii <52634004+iithehangmanii@users.noreply.github.com>
2021-08-12 23:19:10 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,72 @@
+import tensorflow.keras as keras
+from tensorflow.keras.models import load_model
+import numpy as np
+from glob import glob
+from tensorflow.keras.preprocessing import image                  
+from tqdm import tqdm
+import os
+import urllib.request
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'static/uploads/'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = "secret key"
+#run_with_ngrok(app)
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+def path_to_tensor(img_path, width=224, height=224):
+    # loads RGB image as PIL.Image.Image type
+    
+    img = image.load_img(img_path, target_size=(width, height))
+    # convert PIL.Image.Image type to 3D tensor with shape (width, heigth, 3)
+    x = image.img_to_array(img)
+    # convert 3D tensor to 4D tensor with shape (1, width, height, 3) and return 4D tensor
+    return np.expand_dims(x, axis=0)
+
+def paths_to_tensor(img_paths, width=224, height=224):
+    list_of_tensors = [path_to_tensor(img_paths, width, height)]
+    return np.vstack(list_of_tensors)
+    
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	
+@app.route('/')
+def upload_form():
+	return render_template('upload.html')
+
+@app.route('/', methods=['POST'])
+def upload_image():
+    
+	if 'file' not in request.files:
+		flash('No file part')
+		return redirect(request.url)
+	file = request.files['file']
+	if file.filename == '':
+		flash('No image selected for uploading')
+		return redirect(request.url)
+	if file and allowed_file(file.filename):
+		model2 = load_model('trained_modelDNN1.h5')
+		filename = secure_filename(file.filename)
+		print(filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		filename1 = "static/uploads/" + filename  
+		print(filename1)
+		test_tensors = paths_to_tensor(filename1)/255
+		pred=model2.predict(test_tensors)
+		pred=np.argmax(pred);
+		flash('given Image Predicted  = '+ str(pred))
+		return render_template('upload.html', filename=filename)
+	else:
+		flash('Allowed image types are -> png, jpg, jpeg, gif')
+		return redirect(request.url)
+
+@app.route('/display/<filename>')
+def display_image(filename):
+	#print('display_image filename: ' + filename)
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+if __name__ == "__main__":
+    app.run()
